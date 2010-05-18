3443dde2afc8745a7aa4beb44afbc3d7ad3c69d2
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2010-05-17 22:14:57 -0300

    Chapter 5 - Adding Franc and its multiplication test. Copy and paste. Duplication.

---
+++ src/test/java/com/aquiles/alexandre/money/Franc.java
@@ -0,0 +1,22 @@
+package com.aquiles.alexandre.money;
+
+public class Franc {
+
+	private int amount;
+
+	public Franc(int amount) {
+		this.amount = amount;
+	}
+
+	public Franc times(int multiplier) {
+		return new Franc(amount *  multiplier);
+		
+	}
+	
+	@Override
+	public boolean equals(Object object) {
+		Franc dollar = (Franc) object;
+		return this.amount == dollar.amount;
+	}
+
+}
--- src/test/java/com/aquiles/alexandre/money/MoneyTest.java
+++ src/test/java/com/aquiles/alexandre/money/MoneyTest.java
@@ -9,6 +9,12 @@ public class MoneyTest extends TestCase {
 		assertEquals(new Dollar(15), five.times(3));
 	}
 	
+	public void testFrancMultiplication(){
+		Franc five = new Franc(5);
+		assertEquals(new Franc(10), five.times(2));
+		assertEquals(new Franc(15), five.times(3));
+	}
+	
 	public void testEquality(){
 		assertTrue(new Dollar(5).equals(new Dollar(5)));
 		assertFalse(new Dollar(5).equals(new Dollar(6)));
