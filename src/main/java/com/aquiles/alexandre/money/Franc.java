e9cf4c198544af816fa7aa6d0b584386b1a1a341
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2010-05-18 21:06:55 -0300

    Chapter 8 - Using factory methods to decouple test code from model. Making Money abstract. Pulling up times().

--- src/main/java/com/aquiles/alexandre/money/Franc.java
+++ src/main/java/com/aquiles/alexandre/money/Franc.java
@@ -2,12 +2,12 @@ package com.aquiles.alexandre.money;
 
 public class Franc extends Money {
 
-	public Franc(int amount) {
+	Franc(int amount) {
 		this.amount = amount;
 	}
 
-	public Franc times(int multiplier) {
-		return new Franc(amount *  multiplier);
+	public Money times(int multiplier) {
+		return Money.franc(amount *  multiplier);
 		
 	}
 	
--- src/main/java/com/aquiles/alexandre/money/Money.java
+++ src/main/java/com/aquiles/alexandre/money/Money.java
@@ -1,6 +1,6 @@
 package com.aquiles.alexandre.money;
 
-public class Money {
+abstract public class Money {
 	protected int amount;
 
 	public boolean equals(Object object) {
@@ -8,4 +8,14 @@ public class Money {
 		return this.amount == money.amount
 		&& getClass().equals(money.getClass());
 	}
+
+	public static Money dollar(int amount) {
+		return new Dollar(amount);
+	}
+
+	public static Franc franc(int amount) {
+		return new Franc(amount);
+	}
+
+	abstract public Money times(int amount);
 }
