6f1892b4ea1b3d72801c51470dac1c3c07356ae7
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2010-06-03 21:18:49 -0300

    Chapter 14 - Implementing rate conversions.

--- src/main/java/com/aquiles/alexandre/money/Bank.java
+++ src/main/java/com/aquiles/alexandre/money/Bank.java
@@ -1,9 +1,68 @@
 package com.aquiles.alexandre.money;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank {
 
+	private Map<Pair, Integer> rates = new HashMap<Pair, Integer>(); 
+	
 	Money reduce(Expression source, String to) {
-		return source.reduce(to);
+		return source.reduce(this, to);
+	}
+
+	void addRate(String from, String to, int rate) {
+		rates.put(new Pair(from, to), rate);
+	}
+	
+	int rate(String from, String to) {
+		if(from.equals(to)) 
+			return 1;
+		return rates.get(new Pair(from, to));
+	}
+
+	private class Pair {
+		private String from;
+		private String to;
+
+		public Pair(String from, String to) {
+			this.from = from;
+			this.to = to;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((from == null) ? 0 : from.hashCode());
+			result = prime * result + ((to == null) ? 0 : to.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			final Pair other = (Pair) obj;
+			if (from == null) {
+				if (other.from != null)
+					return false;
+			} else if (!from.equals(other.from))
+				return false;
+			if (to == null) {
+				if (other.to != null)
+					return false;
+			} else if (!to.equals(other.to))
+				return false;
+			return true;
+		}
+
+		
+		
 	}
 
 }
--- src/main/java/com/aquiles/alexandre/money/Expression.java
+++ src/main/java/com/aquiles/alexandre/money/Expression.java
@@ -2,6 +2,6 @@ package com.aquiles.alexandre.money;
 
 public interface Expression {
 
-	Money reduce(String to);
+	Money reduce(Bank bank, String to);
 
 }
